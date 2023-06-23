f1f43195e5844af25a619fa80009b6f3340a08e9
Balasubramanian Narasimhan <b.naras@gmail.com>
2023-06-22 17:13:56 -0700

    Some fixes for checks

--- src/rust/vendor.sh
+++ src/rust/vendor.sh
@@ -5,39 +5,6 @@
 
 cargo vendor
 
-echo
-echo "Checking for files with missing newlines at end"
-
-## Some files in Lapack are missing newlines resulting in notes.
-## So fix them.
-rm -f ./modified_files.txt
-## define the directory to start from
-startdir=./vendor/lapack-sys/lapack
-
-## -type f means 'find files only', not directories
-## -print0 and -0 handle file names with spaces
-## -name "*.f" -o -name "*.c" matches files ending with .f or .c
-find "$startdir" -type f \( -name "*.f" -o -name "*.c" \) -print0 | while IFS= read -r -d '' file; do
-    # if the file does not end with a newline
-    if [ "$(tail -c 1 "$file" | wc -l)" -eq 0 ]; then
-        # append a newline to the end of the file
-        printf '\n' >> "$file"
-	# print what was done to console
-	printf "Appended newline to $file\n"
-	## Append to list of modified files
-	printf "$file\n" >> ./modified_files.txt
-    fi
-done
-
-## Fix hashes
-if [ -f "./modified_files.txt" ]
-then
-    echo
-    echo "Modifying SHA256 hashes to reflect changed files"
-    ./fix_hash.R
-    rm -f ./modified_files.txt
-fi
-
 # c.f. https://reproducible-builds.org/docs/archives/
 echo
 echo "Tarring up files"
